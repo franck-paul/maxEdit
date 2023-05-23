3fdcb44e12f843162bd46239970a70f1da1f0e25
franck <carnet.franck.paul@gmail.com>
2023-05-23 09:54:17 +0200

    l10n

---
+++ locales/en/main.lang.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * @package Dotclear
+ *
+ * @copyright Olivier Meunier & Association Dotclear
+ * @copyright GPL-2.0-only
+ */
+#
+# DOT NOT MODIFY THIS FILE !
+#
+
+use Dotclear\Helper\L10n;
+
+L10n::$locales['Maximize mode for dcLegacyEditor'] = '';
+L10n::$locales['maxEdit'] = '';
+L10n::$locales['Switch to maximized mode'] = '';
+L10n::$locales['Exit from maximized mode'] = '';
