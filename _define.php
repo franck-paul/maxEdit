23af68c5d11ad288e216f2993d4145b7874bcb86
franck <carnet.franck.paul@gmail.com>
2024-08-27 08:52:52 +0200

    New release 4.2

--- _define.php
+++ _define.php
@@ -14,7 +14,7 @@ $this->registerModule(
     'maxEdit',
     'Maximize mode for dcLegacyEditor',
     'Franck Paul',
-    '4.1',
+    '4.2',
     [
         'requires'    => [['core', '2.28']],
         'permissions' => 'My',
