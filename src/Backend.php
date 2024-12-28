3f2a077b9c08bf727dc403351b109de082381188
franck <carnet.franck.paul@gmail.com>
2024-12-28 05:23:08 +0100

    PHP Code review (Rector/PHPStan)

--- src/Backend.php
+++ src/Backend.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * @brief maxEdit, a plugin for Dotclear 2
  *
@@ -22,7 +23,8 @@ class Backend extends Process
     public static function init(): bool
     {
         // dead but useful code, in order to have translations
-        __('maxEdit') . __('Maximize mode for dcLegacyEditor');
+        __('maxEdit');
+        __('Maximize mode for dcLegacyEditor');
 
         return self::status(My::checkContext(My::BACKEND));
     }
